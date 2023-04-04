7d6584b34730426df6773701426fd7d86ecd42bb
Ted Hwang <htedd10@gmail.com>
2023-04-04 23:38:02 +0000

    Create, update, and delete endpoints for cart items table. Get a shopping cart w/cart items detail endpoint as well.

---
+++ meal-ting-pot/routers/cart_items.py
@@ -0,0 +1,41 @@
+from fastapi import APIRouter, Depends, Response
+from typing import Union, Optional, List
+from queries.cart_items import (
+    Error,
+    CartItemIn,
+    CartItemOut,
+    CartItemRepository,
+)
+from authenticator import authenticator
+
+router = APIRouter()
+
+
+@router.post("/cart_item", response_model=Union[CartItemOut, Error])
+def create_cart_item(
+    cart_item: CartItemIn,
+    response: Response,
+    repo: CartItemRepository = Depends(),
+    account_data: dict = Depends(authenticator.get_current_account_data),
+):
+    response.status = 400
+    return repo.create(cart_item)
+
+
+@router.put("/cart_item/{id}", response_model=Union[CartItemOut, Error])
+def update_cart_item(
+    id: int,
+    cart_item: CartItemIn,
+    repo: CartItemRepository = Depends(),
+    account_data: dict = Depends(authenticator.get_current_account_data),
+) -> Union[CartItemOut, Error]:
+    return repo.update(id, cart_item)
+
+
+@router.delete("/cart_item/{id}", response_model=bool)
+def delete_cart_item(
+    id: int,
+    repo: CartItemRepository = Depends(),
+    account_data: dict = Depends(authenticator.get_current_account_data),
+) -> bool:
+    return repo.delete(id)
