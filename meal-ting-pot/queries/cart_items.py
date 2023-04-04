e27b48a3a374a32848f45b76c5ac5401b9904ddb
Ted <htedd10@gmail.com>
2023-04-04 16:25:06 -0700

    Created an update and delete endpoint for cart item. Both are protected to signed in users

--- meal-ting-pot/queries/cart_items.py
+++ meal-ting-pot/queries/cart_items.py
@@ -20,6 +20,46 @@ class CartItemOut(BaseModel):
 
 
 class CartItemRepository:
+    def delete(self, id: int) -> bool:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    db.execute(
+                        """
+                        DELETE from cart_items
+                        WHERE id = %s
+                        """,
+                        [id]
+                    )
+                    return True
+        except Exception as e:
+            print(e)
+            return False
+
+    def update(self, id: int, cart_item: CartItemIn) -> Union[CartItemOut, Error]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    db.execute(
+                        """
+                        UPDATE cart_items
+                        SET shopping_cart_id = %s
+                            , menu_item_id = %s
+                            , quantity = %s
+                        WHERE id = %s;
+                        """,
+                        [
+                            cart_item.shopping_cart_id,
+                            cart_item.menu_item_id,
+                            cart_item.quantity,
+                            id
+                        ]
+                    )
+                    return self.cart_item_in_to_out(id, cart_item)
+        except Exception as e:
+            print(e)
+            return {"message": "Could not update cart item"}
+
     def create(self, cart_item: CartItemIn) -> Union[CartItemOut, Error]:
         try:
             with pool.connection() as conn:
--- meal-ting-pot/routers/cart_items.py
+++ meal-ting-pot/routers/cart_items.py
@@ -20,3 +20,22 @@ def create_cart_item(
 ):
     response.status = 400
     return repo.create(cart_item)
+
+
+@router.put("/cart_item/{id}", response_model=Union[CartItemOut, Error])
+def update_cart_item(
+    id: int,
+    cart_item: CartItemIn,
+    repo: CartItemRepository = Depends(),
+    account_data: dict = Depends(authenticator.get_current_account_data),
+) -> Union[CartItemOut, Error]:
+    return repo.update(id, cart_item)
+
+
+@router.delete("/cart_item/{id}", response_model=bool)
+def delete_cart_item(
+    id: int,
+    repo: CartItemRepository = Depends(),
+    account_data: dict = Depends(authenticator.get_current_account_data),
+) -> bool:
+    return repo.delete(id)
