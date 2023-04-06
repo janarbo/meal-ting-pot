e43360fb3f44b65425356058d79c602446546194
Ted <htedd10@gmail.com>
2023-04-05 23:21:51 -0700

    Revise GET order endpoint to return details from cart item and menu item. Added error handling in the create endpoint where it won't work if the shopping cart is empty. Also, update order will only be able to revise status

--- meal-ting-pot/routers/orders.py
+++ meal-ting-pot/routers/orders.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter, Depends, Response
 from typing import Union, List, Optional
 from queries.orders import (
-    OrdersIn,
+    CreateOrderIn,
+    UpdateOrderIn,
     OrdersRepository,
     OrdersOut,
+    OrdersDetailOut,
     Error,
 )
 
@@ -14,36 +16,29 @@ router = APIRouter()
 
 @router.post("/orders", response_model=Union[OrdersOut, Error])
 def create_order(
-    orders: OrdersIn,
+    orders: CreateOrderIn,
     response: Response,
     repo: OrdersRepository = Depends(),
-    account_data: dict = Depends(authenticator.try_get_current_account_data),
+    account_data: dict = Depends(authenticator.get_current_account_data),
 ):
     response.status = 400
     return repo.create(orders, account_data)
 
-@router.get("/orders", response_model=List[OrdersOut])
+@router.get("/orders", response_model=List[OrdersDetailOut])
 def get_all_orders(
     response: Response,
     repo: OrdersRepository = Depends(),
-    customer_id: Optional[int] = None,
     account_data: dict = Depends(authenticator.get_current_account_data),
-) -> List[OrdersOut]:
-    orders = repo.get_all(customer_id=customer_id)
-    if not orders:
-        response.status_code = 404
+) -> List[OrdersDetailOut]:
+    response.status = 400
+    orders = repo.get_all()
     return orders
 
 @router.put("/orders/{order_id}", response_model=Union[OrdersOut, Error])
 def update_order(
     order_id: int,
-    orders: OrdersIn,
-    response: Response,
+    orders: UpdateOrderIn,
     repo: OrdersRepository = Depends(),
     account_data: dict = Depends(authenticator.get_current_account_data),
 ) -> Union[OrdersOut, Error]:
-    order = repo.get_all(order_id)
-    if order is None:
-        response.status_code = 404
-        return {"message": "Order not found"}
     return repo.update(order_id, orders)
