18398933d83ca301df67d3f1951e719be3a6f378
Derek <dwang963@gmail.com>
2023-04-06 18:52:00 -0700

    some basic login functionality working

--- ghi/src/store/authAPI.js
+++ ghi/src/store/authAPI.js
@@ -4,21 +4,60 @@ export const authApi = createApi({
     reducerPath: 'authentication',
     baseQuery: fetchBaseQuery({
         baseUrl: process.env.REACT_APP_MEAL_TING_POT_API_HOST,
+        credentials: "include",
+        prepareHeaders: (headers, { getState }) => {
+    const token = getState().token
+    console.log(token)
+
+    // If we have a token set in state, let's assume that we should be passing it.
+    if (token) {
+        headers.set('authorization', `Bearer ${token}`)
+        console.log('imloggedin')
+    }
+
+    return headers
+    },
     }),
     endpoints: builder => ({
-        signup: builder.query({
+        signup: builder.mutation({
             query: () => '/api/accounts',
         }),
-        login: builder.query({
+        login: builder.mutation({
+            query: info => {
+        let formData = null;
+        if (info instanceof HTMLElement) {
+            formData = new FormData(info);
+        } else {
+            formData = new FormData();
+            formData.append('username', info.username);
+            formData.append('password', info.password);
+        }
+        return {
+            url: '/token',
+
+            method: 'post',
+            body: formData,
+            credentials: 'include',
+        };
+        },
+        invalidatesTags: result => {
+        return (result && ['accounts']) || [];
+        },
+    }),
+    getToken: builder.query({
+        query: () => ({
+        url: '/token',
+
+        credentials: 'include',
+        }),
+        providesTags: ['Token'],
+
+
+        logout: builder.mutation({
             query: () => '/token',
         }),
-        logout: builder.query({
-            query: () => '/token',
-        }),
-        getToken: builder.query({
-            query: () => '/token'
-        })
+}),
     }),
 });
 
-export const { useAuthQuery } = authApi;
+export const { useAuthQuery, useLoginMutation } = authApi;
--- ghi/src/store/store.js
+++ ghi/src/store/store.js
@@ -1,14 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { setupListeners } from '@reduxjs/toolkit/query'
-import { authApi } from './authAPI';
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/dist/query";
+import { authApi } from "./authAPI";
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
     [authApi.reducerPath]: authApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware()
+      .concat(authApi.middleware),
+});
 
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware().concat(authApi.middleware),
-})
-
-setupListeners(store.dispatch)
+setupListeners(store.dispatch);
+export default store
