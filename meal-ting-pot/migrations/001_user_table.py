34eb2164ddd8a6b1cd5b99288a36676a27de1815
Ted <htedd10@gmail.com>
2023-04-12 03:53:37 -0700

    Change user table database to match main branch

--- meal-ting-pot/migrations/001_user_table.py
+++ meal-ting-pot/migrations/001_user_table.py
@@ -6,9 +6,9 @@ steps = [
             id SERIAL PRIMARY KEY NOT NULL,
             first_name VARCHAR(100) NOT NULL,
             last_name VARCHAR(100) NOT NULL,
-            username VARCHAR(18) NOT NULL,
+            username VARCHAR(18) NOT NULL UNIQUE,
             hashed_password VARCHAR(250),
-            email VARCHAR(40) NOT NULL,
+            email VARCHAR(40) NOT NULL UNIQUE,
             is_chef BOOL NOT NULL
         );
         """,
