fbaee6e7fdea971c0a8609756c2e9bd65a8c5f58
Ted Hwang <htedd10@gmail.com>
2023-05-03 18:48:58 +0000

    Test deployment url in checkout fetch request

--- ghi/src/components/shopping-cart/ShoppingCartList.js
+++ ghi/src/components/shopping-cart/ShoppingCartList.js
@@ -15,10 +15,10 @@ function ShoppingCartList() {
     const [createCartItem] = useCreateCartItemMutation();
     const [createOrder] = useCreateOrderMutation();
 
-    const apiUrl = process.env.API_URL || 'http://localhost:4000';
+    // const apiUrl = process.env.API_URL || 'http://localhost:4000';
 
     const checkout = async () => {
-        await fetch(`${apiUrl}/checkout`, {
+        await fetch(`https://bidoof_supremacy.gitlab.io/meal-ting-pot/checkout`, {
             method: "POST",
             headers: {
                 'Content-Type': 'application/json'
